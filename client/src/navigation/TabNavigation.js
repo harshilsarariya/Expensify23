6297fd635bf044d2054d288f548ee83b35ee5712
Harshil Sarariya <harshilsarariya@users.noreply.github.com>
2023-02-18 20:56:03 +0530

    tab--

--- client/src/navigation/TabNavigation.js
+++ client/src/navigation/TabNavigation.js
@@ -62,16 +62,12 @@ const TabNavigation = () => {
           )}
         </Tab.Screen>
         <Tab.Screen name="Split" options={{ tabBarStyle: { display: "none" } }}>
-          {(props) => (
-            <SplitNavigation
-              navigation={navigation}
-            />
-          )}
+          {(props) => <SplitNavigation navigation={navigation} />}
         </Tab.Screen>
       </Tab.Navigator>
 
       {/* Tab navigation */}
-      {tabShown && (
+      {/* {tabShown && (
         <View className="bg-[#2A2E39] flex flex-row justify-between py-3 px-16 rounded-tl-3xl rounded-r-3xl  absolute bottom-0 w-full">
           <TouchableOpacity
             className="items-center"
@@ -88,7 +84,7 @@ const TabNavigation = () => {
             <Text className="text-[#6561D2] text-xs mt-1">Split</Text>
           </TouchableOpacity>
         </View>
-      )}
+      )} */}
     </>
   );
 };
