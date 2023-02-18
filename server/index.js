be4c966f3a07171207a382647aa6cca3c068ba51
t3j3ndrA <tejendradhanani@gmail.com>
2023-02-18 15:38:09 +0530

    Users routes created

--- server/index.js
+++ server/index.js
@@ -5,15 +5,25 @@ const dotenv = require("dotenv");
 // database configurations
 require("./database/index");
 
+// routes import
+
+const userRoute = require("./routes/user.route");
+const tailwoRoute = require("./routes/twilio.router");
+
 // enviorment variables configurations
 dotenv.config();
 
-// test route
+app.use(express.json());
 
+// test route
 app.get("/", (req, res) => {
 	res.json({ msg: "Server is up and running" });
 });
 
+// middlewares
+
+app.use("/api/user", userRoute);
+app.use("/api", tailwoRoute);
 app.listen(process.env.PORT, () => {
 	console.log("server is listening on port : " + process.env.PORT);
 });
