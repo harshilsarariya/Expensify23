491ea724dbf8531f9d35d03516b1bc8e71f4241c
t3j3ndrA <tejendradhanani@gmail.com>
2023-02-18 11:06:41 +0530

    Server is up and running

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+
+// enviorment variables configurations
+dotenv.config();
+
+// test route
+
+app.get("/", (req, res) => {
+	res.json({ msg: "Server is up and running" });
+});
+
+app.listen(process.env.PORT, () => {
+	console.log("server is listening on port : " + process.env.PORT);
+});
