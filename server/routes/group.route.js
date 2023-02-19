bdc7a3b08b60b9ca06a9f20ff8740f25affbbc99
t3j3ndrA <tejendradhanani@gmail.com>
2023-02-19 10:50:29 +0530

    settle up transactions done

--- server/routes/group.route.js
+++ server/routes/group.route.js
@@ -200,13 +200,41 @@ router.post("/:grpId/tx/:txId/update", (req, res) => {
 });
 
 // # get all groups
-router.get("/all", (req, res) => {
+router.get("/get/all", (req, res) => {
 	GroupModel.find({}).then((result) => {
 		return res.json({ success: true, data: result });
 	});
 });
 
 // settle transaction
-router.post("/:grpId/tx/settle", (req, res) => {});
+router.post("/:grpId/tx/settle/:ofUser/:withUser", async (req, res) => {
+	const { grpId, ofUser, withUser } = req.params;
+	console.log("g, o, w", grpId, ofUser, withUser);
+	const allTxs = await GroupModel.findOne({ _id: grpId }).select("txs");
+	let total = 0;
+	if (allTxs) {
+		allTxs.txs.forEach((txItem) => {
+			if (txItem.paidBy == ofUser) {
+				txItem.withUsers.forEach((wu) => {
+					if (wu.userId == withUser) {
+						total += wu.owe;
+					}
+				});
+			} else if (txItem.paidBy == withUser) {
+				txItem.withUsers.forEach((wu) => {
+					if (wu.userId == ofUser) {
+						total -= wu.owe;
+					}
+				});
+			}
+		});
+	}
+
+	return res.json({
+		succes: true,
+		data: total,
+		linkToPay: `upi://pay?pa=harshilprajapati9192@okicici&am=${abs(total)}`,
+	});
+});
 
 module.exports = router;
