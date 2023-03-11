b17a1ef8b82eb183d2a3ec136397e2f04c097274
Harshil Sarariya <harshilsarariya@users.noreply.github.com>
2023-03-11 09:44:46 +0530

    added bg image

--- client/src/screens/Split/GroupChat.js
+++ client/src/screens/Split/GroupChat.js
@@ -1,16 +1,22 @@
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { useEffect, useState } from "react";
-import { View, Modal, TouchableOpacity, ScrollView } from "react-native";
+import {
+  View,
+  Modal,
+  TouchableOpacity,
+  ScrollView,
+  ImageBackground,
+} from "react-native";
 import { Text, Image } from "react-native";
 
 import GeneralNavbar from "../../components/GeneralNavbar";
 import ChatCard from "../../components/split/ChatCard";
 import * as Linking from "expo-linking";
-import CommonNav from "./CommonNav";
 import { FontAwesome } from "@expo/vector-icons";
-import { AntDesign } from "@expo/vector-icons";
 import { grpSettle } from "../../api/group";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import bgImg from "../../assets/images/bg-dark.jpg";
+
 const GroupChat = () => {
   const route = useRoute();
   const { item } = route.params;
@@ -24,10 +30,6 @@ const GroupChat = () => {
   const handleUserId = async () => {
     setUserId(await AsyncStorage.getItem("userId"));
   };
-
-  useEffect(() => {
-    handleUserId();
-  }, []);
 
   const handleSettle = async () => {
     const { data, linkToPay } = await grpSettle(
@@ -51,6 +53,9 @@ const GroupChat = () => {
       console.log(error);
     }
   };
+  useEffect(() => {
+    handleUserId();
+  }, []);
 
   useEffect(() => {
     handleSettle();
@@ -58,99 +63,115 @@ const GroupChat = () => {
 
   return (
     <>
-      <GeneralNavbar title={"Group Name"} navigationPath={"MainSplit"} />
-      <View className="p-4 pb-28 h-screen">
-        <Modal
-          animationType="slide"
-          transparent={false}
-          visible={ModalOpen}
-          onRequestClose={() => setModalOpen(false)}
-        >
-          <View className="bg-slate-800 h-screen p-8">
-            <View className="flex flex-row justify-between">
-              <Text className="text-white text-2xl font-medium">
-                Settle Balances
-              </Text>
-              <TouchableOpacity onPress={() => setModalOpen(false)}>
-                <Text className="font-medium text-xl text-purple-400">
-                  Done
+      <GeneralNavbar title={item.name} navigationPath={"MainSplit"} />
+      <ImageBackground
+        source={bgImg}
+        resizeMode="cover"
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          backgroundColor: "#8a3a3a",
+        }}
+      >
+        <View className="p-4 pb-28 h-screen">
+          <Modal
+            animationType="slide"
+            transparent={false}
+            visible={ModalOpen}
+            onRequestClose={() => setModalOpen(false)}
+          >
+            <View className="bg-[#2A2E39] h-screen p-4">
+              <View className="flex flex-row justify-between">
+                <Text className="text-white text-2xl font-medium">
+                  Settle Balances
                 </Text>
-              </TouchableOpacity>
-            </View>
-            <View className="my-4">
-              {item.members.map((val, idx) => {
-                return (
-                  <>
-                    <View className="p-4 flex flex-row justify-between items-center">
-                      <View className="flex flex-row  flex-1 space-x-4">
-                        <View>
-                          <Image
-                            className="w-12 h-12 rounded-full"
-                            source={require("../../assets/images/avatar2.png")}
-                          />
+                <TouchableOpacity onPress={() => setModalOpen(false)}>
+                  <Text className="font-medium text-xl text-[#8885ea]">
+                    Done
+                  </Text>
+                </TouchableOpacity>
+              </View>
+              <View className="my-4">
+                {item.members.map((val, idx) => {
+                  return (
+                    <>
+                      <View className="p-4 flex flex-row justify-between items-center">
+                        <View className="flex flex-row  flex-1 space-x-4 items-center">
+                          <View>
+                            <Image
+                              className="w-12 h-12 rounded-full"
+                              source={require("../../assets/images/avatar2.png")}
+                            />
+                          </View>
+                          <View>
+                            <Text className="text-white font-medium">
+                              {val.substr(15)}
+                            </Text>
+                            <Text className="text-emerald-400 text-base">
+                              <FontAwesome
+                                name="rupee"
+                                size={14}
+                                color="#34d399"
+                              />{" "}
+                              {settleData.data}
+                            </Text>
+                          </View>
                         </View>
-                        <View>
-                          <Text className="text-white font-medium">
-                            {val.substr(15)}
+                        <TouchableOpacity
+                          onPress={() => {
+                            handleOnLinkPress(settleData.linkToPay);
+                            setModalOpen(false);
+                            navigation.navigate("PaySettle");
+                          }}
+                          className="bg-[#2f3647] p-2 rounded-xl"
+                        >
+                          <Text className="text-center px-3 text-[#8885ea]">
+                            Settle
                           </Text>
-                          <Text className="text-emerald-400 text-lg">
-                            <FontAwesome
-                              name="rupee"
-                              size={20}
-                              color="#34d399"
-                            />{" "}
-                            {settleData.data}
-                          </Text>
-                        </View>
+                        </TouchableOpacity>
                       </View>
-                      <TouchableOpacity
-                        onPress={() => {
-                          handleOnLinkPress(settleData.linkToPay);
-                          setModalOpen(false);
-                          navigation.navigate("PaySettle");
-                        }}
-                        className="bg-slate-700 p-2 rounded-xl"
-                      >
-                        <Text className="text-center text-white">Settle</Text>
-                      </TouchableOpacity>
-                    </View>
-                  </>
-                );
-              })}
-              {/* <View className="flex flex-row justify-between">
+                    </>
+                  );
+                })}
+                {/* <View className="flex flex-row justify-between">
                 <Text className="text-white font-medium">Total</Text>
                 <Text className="text-emerald-400 text-2xl">
                   <FontAwesome name="rupee" size={24} color="#34d399" /> 55.4
                 </Text>
               </View> */}
+              </View>
             </View>
+          </Modal>
+          <ScrollView className="mt-3" showsVerticalScrollIndicator={false}>
+            <View className="flex flex-col">
+              {chatData.map((chatItem, idx) => {
+                return (
+                  <>
+                    <View className="my-2" key={idx}>
+                      <ChatCard item={chatItem} />
+                    </View>
+                  </>
+                );
+              })}
+            </View>
+          </ScrollView>
+          <View className="mb- flex flex-row space-x-5 mx-auto">
+            <TouchableOpacity
+              className="font-medium p-2 w-2/5 rounded-lg bg-[#6561D2] uppercase py-2 text-center"
+              onPress={() => {
+                setModalOpen(true);
+              }}
+            >
+              <Text className="text-[#d6d9e3] text-base mx-auto">₹ Settle</Text>
+            </TouchableOpacity>
+            <TouchableOpacity className="font-medium rounded-lg p-2 w-2/5  bg-[#6561D2] uppercase py-2 text-center">
+              <Text className="text-[#d6d9e3] text-base mx-auto">
+                + Expense
+              </Text>
+            </TouchableOpacity>
           </View>
-        </Modal>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <View className="flex flex-col">
-            {chatData.map((chatItem, idx) => {
-              return (
-                <View>
-                  <View className="my-2" key={idx}>
-                    <ChatCard item={chatItem} />
-                  </View>
-                </View>
-              );
-            })}
-          </View>
-        </ScrollView>
-        <TouchableOpacity
-          onPress={() => {
-            setModalOpen(true);
-            console.log("asdns");
-          }}
-          className="bg-indigo-500 rounded-lg w-full mb-3"
-        >
-          <Text className="font-medium uppercase py-2 text-center  text-white">
-            Settle Up
-          </Text>
-        </TouchableOpacity>
-      </View>
+        </View>
+      </ImageBackground>
     </>
   );
 };
